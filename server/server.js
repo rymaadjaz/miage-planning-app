fe278c2b23c18a412c9e18ca450148e8c258baea
Edris-Y <Edrisyoussef32@gmail.com>
2026-01-29 21:11:16 +0100

    Test commit branch Edris

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
-
 app.get('/', (req, res) => {
   res.send('API Gestion Planning - MIAGE en ligne');
 });
